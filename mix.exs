354e65b69ea7e9b70c6a43a62adce3217637aa6e
shura <shura1991@gmail.com>
2013-08-02 22:22:47 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -15,6 +15,6 @@ defmodule OOP.Mixfile do
   # Returns the list of dependencies in the format:
   # { :foobar, "0.1", git: "https://github.com/elixir-lang/foobar.git" }
   defp deps do
-    [ { :managed_process, "0.0.1", github: "meh/elixir-managed_process" } ]
+    [ { :managed_process, github: "meh/elixir-managed_process" } ]
   end
 end
